db889ce5f0d437c70bafb6c9839c416a753a2eb5
Rommel <68463203+rommelsim@users.noreply.github.com>
2025-02-11 17:41:18 +0800

    try this

--- Blade18.py
+++ Blade18.py
@@ -3,7 +3,7 @@ from driver import SynapseWebDriverClass
 from robot.api.deco import keyword
 
 def PerformChromaTestBlade18():
-    lighting_selector = '#root > div > div.nav-tabs > div.navs-wrapper > div:nth-child(6)'
+    lighting_selector = '#root > div > div.nav-tabs > div.navs-wrapper > div:nth-child(7)'
     chroma_effect_dropdown_menu_selector = '#body-wrapper > div > div > div.widget-col.col-right.flex > div > div > div:nth-child(2) > div.modes-area.active > div.flex.chroma-flex-row > div.dropdown-area > div.s3-dropdown'
     chroma_effect_static_selector = '#body-wrapper > div > div > div.widget-col.col-right.flex > div > div > div:nth-child(2) > div.modes-area.active > div.flex.chroma-flex-row > div.dropdown-area > div.s3-options.unsetZ.flex > div:nth-child(9)'
     driver = SynapseWebDriverClass()
